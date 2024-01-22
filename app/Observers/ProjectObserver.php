85fd6b337c65623c725971dc044e3d03c104524b
Allen Wilson <a_m_wilson@icloud.com>
2024-01-22 17:19:25 -0500

    Lesson 21

--- app/Observers/ProjectObserver.php
+++ app/Observers/ProjectObserver.php
@@ -2,32 +2,17 @@
 
 namespace App\Observers;
 
-use App\Models\Activity;
 use App\Models\Project;
 
 class ProjectObserver
 {
-    /**
-     * Handle the Project "created" event.
-     */
     public function created(Project $project): void
     {
-        $this->recordActivity($project, 'created');
+        $project->recordActivity('created');
     }
 
-    /**
-     * Handle the Project "updated" event.
-     */
     public function updated(Project $project): void
     {
-        $this->recordActivity($project, 'updated');
-    }
-
-    protected function recordActivity($project, $type)
-    {
-        Activity::create([
-            'project_id' => $project->id,
-            'description' => $type
-        ]);
+        $project->recordActivity('updated');
     }
 }
--- tests/Feature/ActivityFeedTest.php
+++ tests/Feature/ActivityFeedTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Project;
+use App\Models\Task;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -14,22 +15,54 @@ class ActivityFeedTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function creating_a_project_generates_activity()
+    public function creating_a_project_records_activity()
     {
         $project = Project::factory()->create();
 
         $this->assertCount(1, $project->activity);
-        $this>assertEquals('created', $project->activity[0]->description);
+        $this > assertEquals('created', $project->activity[0]->description);
     }
 
     /** @test */
-    public function updating_a_project_generates_activity()
+    public function updating_a_project_records_activity()
     {
         $project = Project::factory()->create();
-        
+
         $project->update(['title' => 'Changed']);
 
         $this->assertCount(2, $project->activity);
         $this->assertEquals('updated', $project->activity->last()->description);
+    }
+
+    /** @test */
+    public function creating_a_new_task_records_project_activity()
+    {
+        $project = Project::factory()->create();
+
+        $project->addTask('Some task');
+
+        $this->assertCount(2, $project->activity);
+        $this->assertEquals('created_task', $project->activity->last()->description);
+    }
+
+    /** @test */
+    public function completing_a_task_records_project_activity()
+    {
+        $project = Project::factory()->create();
+
+        Task::factory()->create([
+            'project_id' => $project->id
+        ]);
+
+        $this->actingAs($project->owner)
+            ->patch($project->tasks[0]->path(), [
+                'body' => 'foobar',
+                'completed' => true
+            ]);
+
+        // dd($project->activity);
+
+        $this->assertCount(3, $project->activity);
+        $this->assertEquals('completed_task', $project->activity->last()->description);
     }
 }
